74b4277838ebfae5f2f346d3dabb1f42acb6d4d9
Carlos Guevara <juancguv@gmail.com>
2025-03-20 15:59:15 -0500

    Implement theme provider and integrate dark mode support

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ function App() {
 
   return (
     <>
-        <h1 className="text-3xl text-red-900 font-bold underline">
-          Hello world!
-        </h1>
+      <h1 className="text-3xl text-red-900 font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
