8c40a05bf565717b096e4c3cb9e6f73abc4cd4e4
Carlos Guevara <juancguv@gmail.com>
2025-03-20 20:18:41 -0500

    Add Home component

---
+++ src/app/Home.tsx
@@ -0,0 +1,14 @@
+
+export const Home = () => {
+  return (
+    <section id="home" className="container text-center mx-auto my-4 mt-0 pt-[15rem] pb-[20rem]">
+      <h1 className="text-cyan-500 text-2xl sm:text-4xl mb-5 font-bold h-8">
+        Hola! 👋, soy
+      </h1>
+      <h1 className="text-zinc-100 text-4xl sm:text-6xl mb-5 font-bold">Carlos Guevara</h1>
+      <h2 className="text-zinc-300 text-4xl sm:text-5xl mb-4 font-bold h-12">
+        Desarrollador Web Fullstack
+      </h2>
+    </section>
+  )
+}
