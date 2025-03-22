57441c79de3de527db3fefe2d168136398194006
Carlos Guevara <juancguv@gmail.com>
2025-03-22 03:07:12 -0500

    Add technology icons update Projects component to include new tags

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -4,34 +4,32 @@ import { Icons } from "@/assets/icons";
 
 export const Projects = () => {
 
-  const tags: Array<keyof typeof Icons> = ['node', 'react'];
+  const tags: Array<keyof typeof Icons> = ['laravel','csharp','jquery', 'css', 'typescript', 'html', 'angular'];
 
   return (
     <section>
         <SpotlightCard>
             <div className=''>
-            <article>
-
-        <div className="flex flex-col gap-2 px-4 pt-4 pb-6">
-
-          <p className="mb-2 ml:text-lg text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores mollitia dolorum, ipsam illo aut ipsa ex repellat placeat, enim cupiditate, voluptatibus ullam voluptates quod. Labore autem consequatur aut distinctio laboriosam.</p>
-          <div className="flex flex-wrap items-center gap-2">
-            {tags.map(tag => {
-              const Icon = Icons[tag];
-              return (
-                <Badge
-                  key={tag}
-                  variant="secondary"
-                  className="flex shrink-0 items-center justify-center gap-2 px-2 py-1 font-medium"
-                >
-                  { Icon && <Icon size={16} className="w-4 h-4" /> }
-                  <span className="ml:text-sm text-accent-foreground">{tag}</span>
-                </Badge>
-              )
-            })}
-          </div>
-        </div>
-      </article>
+              <div className="flex flex-col gap-2 px-4 pt-4 pb-6">
+                <p className="mb-2 ml:text-lg text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores mollitia dolorum, ipsam illo aut ipsa ex repellat placeat, enim cupiditate, voluptatibus ullam voluptates quod. Labore autem consequatur aut distinctio laboriosam.</p>
+                <div className="flex flex-wrap items-center gap-2">
+                  { 
+                    tags.map(tag => {
+                      const Icon = Icons[tag];
+                      return (
+                        <Badge
+                          key={tag}
+                          variant="secondary"
+                          className="flex shrink-0 items-center justify-center gap-2 px-2 py-1 font-medium"
+                          >
+                          { Icon && <Icon size={16} className="w-4 h-4" /> }
+                          <span className="ml:text-sm text-accent-foreground">{tag}</span>
+                        </Badge>
+                      )
+                    })
+                  }
+                </div>
+              </div>
             </div>
         </SpotlightCard>
     </section>
--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,13 +1,22 @@
 import { Github } from "./social-media/github"
 import { Linkedin } from "./social-media/linkedin"
 
+import { Angular } from "./technologies/angular"
+import { Bootstrap } from "./technologies/bootstap"
+import { CSharp } from "./technologies/csharp"
+import { Css } from "./technologies/css"
 import { Docker } from "./technologies/docker"
 import { Expressjs } from "./technologies/express"
 import { Git } from "./technologies/git"
+import { Html } from "./technologies/html"
+import { JQuery } from "./technologies/jquery"
+import { Laravel } from "./technologies/laravel"
 import { MongoDB } from "./technologies/mongodb"
+import { MySQL } from "./technologies/mysql"
 import { Nest } from "./technologies/nest"
 import { Next } from "./technologies/next"
 import { Nodejs } from "./technologies/node"
+import { PHP } from "./technologies/php"
 import { PostgreSQL } from "./technologies/postgresql"
 import { React } from "./technologies/react"
 import { Sass } from "./technologies/sass"
@@ -20,15 +29,24 @@ import { Vite } from "./technologies/vite"
 
 
 export const Icons = {
+    angular   : Angular,
+    bootstrap : Bootstrap,
+    csharp    : CSharp,
+    css       : Css,
     github    : Github,
     linkedin  : Linkedin,
     docker    : Docker,
     express   : Expressjs,
     git       : Git,
+    html      : Html,
+    jquery    : JQuery,
+    laravel   : Laravel,
     mongodb   : MongoDB,
+    mysql     : MySQL,
     nest      : Nest,
     next      : Next,
     node      : Nodejs,
+    php       : PHP,
     postgresql: PostgreSQL,
     react     : React,
     sass      : Sass,
