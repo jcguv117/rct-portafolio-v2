464c1fc1ccfd3917c6164c2c2022f9ae7bfaa45d
Carlos Guevara <juancguv@gmail.com>
2025-04-10 17:43:37 -0500

    Fix import path for RotatingText component and update About section to use dynamic rotating texts

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -1,21 +1,25 @@
-import RotatingText from "@/shared/components/roating-text";
+import RotatingText from "@/shared/components/rotating-text";
 import { useTranslation } from "react-i18next";
 
 export const About = () => {
   const { t } = useTranslation();
-
+  const rotatingTexts: string[] = t("About.rotatingTexts", {
+    returnObjects: true,
+  }) as unknown as string[]
 
   return (
     <section id="about" className="container mx-auto">
       <div className="container px-8 grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="ml-4 col-span-1">
           <div>
-            <span className="text-2xl text-cyan-600 font-bold">{t('About.profileTitle')}</span>
-            <p className="text-gray-300 mb-5 text-xl text-justify"> 
+            <span className="text-2xl text-primary font-bold">
+              {t('About.profileTitle')}
+            </span>
+            <p className="mb-5 text-xl text-justify"> 
               {t('About.profileText')}
             </p>
             <RotatingText 
-              texts={['hola', 'mundo']}
+              texts={rotatingTexts}
               mainClassName="inline-flex dark:bg-white bg-[hsl(0-0%-98%)] px-1 ml-2 rounded-sm"
               elementLevelClassName="dark:text-black text-white font-bold"
               />
