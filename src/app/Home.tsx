4c4779599186c03e40514bfb025c707ef9c24b2c
Carlos Guevara <juancguv@gmail.com>
2025-04-10 17:43:12 -0500

    Add i18n Home and Skills components, update profile text

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next"
 import { SocialMedia } from "./components/SocialMedia";
 
 export const Home = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <section id="home" className="container text-center mx-auto my-4 font-bold">
@@ -16,7 +16,7 @@ export const Home = () => {
       <div className="m-4 pt-2">
             <a href="" target="_blank" rel="noreferrer"
               className="bg-sky-600 hover:bg-cyan-700 text-white py-2 px-4 rounded-full cursor-pointer">
-              Ver CV
+              {i18n.language === "es" ? "Ver" : "Show"} CV
             </a>
             <SocialMedia />
       </div>
--- src/app/Skills.tsx
+++ src/app/Skills.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/assets/icons";
 import data from "@/data/skils.json"
 import IconWall from "@/shared/components/icon-wall";
+import { useTranslation } from "react-i18next";
  
 interface Skills {
     name: string;
@@ -8,6 +9,7 @@ interface Skills {
 }
 
 export const Skills = () => {
+    const { t } = useTranslation();
     const SkilsData = data.map((skill: Skills) => {
         const Icon = skill.icon ? Icons[skill.icon as keyof typeof Icons] : undefined;
         return {
@@ -18,9 +20,9 @@ export const Skills = () => {
 
   return (
     <section id="skills" className="container mx-auto my-4">
-        <h1 className="text-center md:text-4xl text-2xl font-medium title-font mb-4 text-white">
-                Conocimientos de tecnologías.
-        </h1>
+        <h3 className="text-center md:text-4xl text-2xl font-medium title-font mb-4">
+            { t("Skills.title") }
+        </h3>
         <IconWall items={SkilsData} size="clamp(8rem, 1rem + 10vmin, 20rem)" bgAccentColor="var(--color-primary)" />
     </section>
   )
