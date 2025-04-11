feb370988bb41916875f4f7df5ba5c7027fa0dcb
Carlos Guevara <juancguv@gmail.com>
2025-04-11 15:13:34 -0500

    Update Projects and Footer components with i18n; modify project title in translations

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -6,7 +6,7 @@ import { ProjectCard } from "./components/ProjectCard";
 
 export const Projects = () => {
 
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
   const projects: Project[] = t("Projects.data", {
     returnObjects: true,
   }) as Project[]
@@ -15,7 +15,7 @@ export const Projects = () => {
   return (
     <section id="projects" className="container mx-auto body-font">
       <div>
-          <h1 className="text-center md:text-4xl text-2xl font-medium title-font mb-4">
+          <h1 className="text-center text-primary md:text-4xl text-2xl font-medium title-font mb-4">
             { t("Projects.title") }
           </h1>
           <div className="grid md:grid-cols-2 grid-cols-1 m-4 gap-2">
@@ -30,8 +30,8 @@ export const Projects = () => {
                 <div className="flex justify-center flex-wrap gap-4 py-2 z-[100]">
                     {
                       <>
-                        <ControlButton icon='github' url={project.github} tooltip="Ir a repositorio" />
-                        <ControlButton icon='globe' url={project.website} tooltip="Visitar Sitio Web" />
+                        <ControlButton icon='github' url={project.github} tooltip={`${i18n.language === "es" ? "Ir a repositorio" : "Open repository"}`} />
+                        <ControlButton icon='globe' url={project.website} tooltip={`${i18n.language === "es" ? "Visitar Sitio Web" : "Open WebSite"}`} />
                       </>
                     }
                 </div>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next"
+
 export const Footer = () => {
+  const { i18n } = useTranslation()
+
   return (
     <div className="h-20 bg-transparent font-bold text-center">
         <hr className="h-[1px] mx-[10%] border border-gray-300 dark:border-gray-700" />
         <p className="pt-4 my-2">
-            <span className="ml-2 text-primary">© Portafolio 2025</span> <br/> 
+            <span className="ml-2 text-primary">© {`${i18n.language === "es" ? "Portafolio" : "Portfolio"}`} 2025</span> <br/> 
         </p>
     </div>
   )
